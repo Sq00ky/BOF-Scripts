bea07cbc119d6cb40f9259b56b7b35be9a0c6817
Sq00ky <44957111+Sq00ky@users.noreply.github.com>
2023-09-05 19:58:56 -0400

    Update BOF_Fun.cpp

--- BOF_Fun/BOF_Fun.cpp
+++ BOF_Fun/BOF_Fun.cpp
@@ -1,10 +1,35 @@
 #include <Windows.h>
 #include <iostream>
-#include "beacon.h"
+#include "bofdefs.h"
 
+#pragma region error_handling
+#define print_error(msg, hr) _print_error(__FUNCTION__, __LINE__, msg, hr)
+BOOL _print_error(char* func, int line, char* msg, HRESULT hr) {
+#ifdef BOF
+    BeaconPrintf(CALLBACK_ERROR, "(%s at %d): %s 0x%08lx", func, line, msg, hr);
+#else
+    printf("[-] (%s at %d): %s 0x%08lx", func, line, msg, hr);
+#endif // BOF
 
-DECLSPEC_IMPORT INT WINAPI WINUSER$MessageBoxA(HWND hWnd, LPCSTR lpTest, LPCSTR lpCaption, UINT uType);
-
-void go() {
-	WINUSER$MessageBoxA(NULL, "Hello From BOF-land!", "Meterpreter Says...", MB_OK);
+    return FALSE;
 }
+#pragma endregion
+
+using namespace std;
+#ifdef BOF
+
+
+extern "C" int go() {
+    USER32$MessageBoxA(NULL, "Hello From BOF-land!", "Meterpreter Says...", MB_OK);
+    BeaconPrintf(CALLBACK_OUTPUT, "Success!");
+    return 0;
+}
+
+#else
+
+int main() {
+    go();
+    return 0;
+}
+
+#endif
